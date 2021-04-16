8a10be97273ac9119798f2d096fbe91dbbaab41e
David Carrillo <carrillo.dact13@gmail.com>
2021-04-15 21:25:00 -0700

    new Spring Boot Backend

---
+++ WorkoutBackend/src/main/java/com/revature/workout/WorkoutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutBackendApplication.class, args);
+	}
+
+}
---
+++ WorkoutBackend/src/test/java/com/revature/workout/WorkoutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
