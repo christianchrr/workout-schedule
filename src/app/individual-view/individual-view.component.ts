1c63bb3807e8af7e6bab13229a7dc76a1df05c68
Christian <christianchrr@gmail.com>
2021-04-21 00:17:10 -0400

    adds individual views

--- src/app/individual-view/individual-view.component.ts
+++ src/app/individual-view/individual-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-individual-view',
@@ -7,12 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class IndividualViewComponent implements OnInit {
 
-  @Input() individual: any
-
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.individual)
   }
 
 }
