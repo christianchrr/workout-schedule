40aa60ea5668e8d058628de382edaca5f4fb515f
Christian <christianchrr@gmail.com>
2021-04-21 03:07:32 -0400

    adds file selector

--- src/app/add-complete-workout-form/add-complete-workout-form.component.ts
+++ src/app/add-complete-workout-form/add-complete-workout-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FileValidator } from 'ngx-material-file-input';
+import { CompleteWorkout } from '../Models/complete-workout';
 
 @Component({
   selector: 'app-add-complete-workout-form',
@@ -6,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-complete-workout-form.component.css']
 })
 export class AddCompleteWorkoutFormComponent implements OnInit {
+  [x: string]: any;
+
+  // cworkout = new CompleteWorkout('','','','','')
+
 
   constructor() { }
 
